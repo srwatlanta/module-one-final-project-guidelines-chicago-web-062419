2eb12e113ce2b19801691e51e5e3fa7560eba186
srwatlanta <shawnrwheeler@gmail.com>
2019-07-09 11:40:34 -0500

    new CLI file

--- lib/app/models/passenger.rb
+++ lib/app/models/passenger.rb
@@ -1,4 +1,8 @@
 class Passenger < ActiveRecord::Base
     has_many :flights
     has_many :destinations, through: :flights
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
 end
