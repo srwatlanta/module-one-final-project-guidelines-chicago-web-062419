04f81856b19b7675680bd997dbc0c573c15903e0
srwatlanta <shawnrwheeler@gmail.com>
2019-07-09 11:26:30 -0500

    seeds changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,12 @@
 require 'csv'
-# require '../lib/app/models/destination.rb'
-# require '../lib/app/models/passenger.rb'
-# require '../lib/app/models/flight.rb'
 
 passenger_data = Array.new
 flight_data = Array.new
 destination_data = Array.new
 
-# CSV.foreach("db/csv/passengers.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
-#     passenger_data << row.to_hash
-# end
+CSV.foreach("db/csv/passengers.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+    passenger_data << row.to_hash
+end
 
 CSV.foreach("db/csv/flights.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
     flight_data << row.to_hash
@@ -20,9 +17,9 @@ CSV.foreach("db/csv/destinations.csv", { encoding: "UTF-8", headers: true, heade
 end
 
 
-# passenger_data.each do |passenger|
-#     Passenger.create(first_name: passenger[:first_name], last_name: passenger[:last_name], age: passenger[:age], country: passenger[:country])
-# end
+passenger_data.each do |passenger|
+    Passenger.create(first_name: passenger[:first_name], last_name: passenger[:last_name], age: passenger[:age], country: passenger[:country])
+end
 
 
 flight_data.each do |flight|
