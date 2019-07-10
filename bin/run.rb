29be85ad811eb766bad0caf682c57dc9e3a7c013
github username <github email address>
2019-07-10 14:18:53 -0500

    methods for destinations and flights

--- bin/run.rb
+++ bin/run.rb
@@ -4,56 +4,60 @@ require_relative '../lib/app/models/passenger'
 require_relative '../lib/app/models/flight'
 require_relative '../lib/command.rb'
 
-greeting
-main_menu
-menu_choice = input
-    if menu_choice.to_i == 1
-        book_flight_menu
-        booking_choice = input
-        if booking_choice.to_i == 1
-            user_id = sign_in
-            destination_id = pick_destination
-            user = find_user(user_id)
-            user.create_flight(destination_id)
-            user.flight_confirmation
-            main_menu
-        elsif input == 2
-            destination_menu
-        else input == 3
-            main_menu
-        end
-    # elsif input == 2
-    #     enter_passenger_id
-    #     user = find_user
-    #     flight_history_menu
-    #     input
-    #     case input
-    #     when 1
-    #         user.my_flights
-    #         flight_history_menu
-    #     when 2
-    #         user.my_terrain_count
-    #         flight_history_menu
-    #     when 3 
-    #         main_menu
-    #     end
-    # elsif input == 3
-    #     destination_info_menu
-    #     input
-    #     case input
-    #     when 1
-    #         Destination.most_popular
-    #         destination_menu
-    #     when 2
-    #         Destination.highest_rated
-    #         destination_menu
-    #     when 3
-    #         Destination.best_terrain
-    #         destination_menu
-    #     when 4
-    #         main_menu
-    #     end
-    # else
-    #     "I'm sorry please select one of the menu options using a number!"
-    end
+# greeting
+# main_menu
+# menu_choice = input
+#     if menu_choice.to_i == 1
+#         book_flight_menu
+#         booking_choice = input
+#         if booking_choice.to_i == 1
+#             user_id = sign_in
+#             destination_id = pick_destination
+#             user = find_user(user_id)
+#             user.create_flight(destination_id)
+#             user.flight_confirmation
+#             main_menu
+#         elsif input == 2
+#             destination_menu
+#         else input == 3
+#             main_menu
+#         end
+#     # elsif input == 2
+#     #     enter_passenger_id
+#     #     user = find_user
+#     #     flight_history_menu
+#     #     input
+#     #     case input
+#     #     when 1
+#     #         user.my_flights
+#     #         flight_history_menu
+#     #     when 2
+#     #         user.my_terrain_count
+#     #         flight_history_menu
+#     #     when 3 
+#     #         main_menu
+#     #     end
+#     # elsif input == 3
+#     #     destination_info_menu
+#     #     input
+#     #     case input
+#     #     when 1
+#     #         Destination.most_popular
+#     #         destination_menu
+#     #     when 2
+#     #         Destination.highest_rated
+#     #         destination_menu
+#     #     when 3
+#     #         Destination.best_terrain
+#     #         destination_menu
+#     #     when 4
+#     #         main_menu
+#     #     end
+#     # else
+#     #     "I'm sorry please select one of the menu options using a number!"
+#     end
 
+# p Flight.most_visited
+
+
+p Destination.most_popular_terrain
--- lib/app/models/destination.rb
+++ lib/app/models/destination.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Destination < ActiveRecord::Base
     has_many :flights
     has_many :passengers, through: :flights
@@ -8,4 +10,33 @@ class Destination < ActiveRecord::Base
         end
     end
     
-end
+    # helper method for self.most_popular_destinations
+    def self.highest_rating
+        self.order(rating: :desc)
+    end
+
+# not working :(, also there are 6 in the seeded data that are a 5 
+    def self.highest_rated
+        self.highest_rating.first(3)
+    end 
+
+    # def self.highest_rated_destinations_2
+    #     self.group('rating').order('count_all').limit(3)
+    # end 
+
+
+
+    def self.average_terrain_rating
+        average = []
+     ratings = Destination.group(:terrain).average(:rating)
+     ratings.each do |terrain, avg|
+        average << "#{terrain}: #{avg.round(2)}"
+        # binding.pry 
+    end 
+    average 
+end 
+    
+end 
+
+# Destination.where(terrain: 'beach').average(:rating)
+
