0325c21ff3e6f10e75a7ecee4209949b38590489
srwatlanta <shawnrwheeler@gmail.com>
2019-07-11 16:04:18 -0500

    sutff

--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,7 @@
-require_relative '../config/environment'
-require_relative '../lib/app/models/destination'
-require_relative '../lib/app/models/passenger'
-require_relative '../lib/app/models/flight'
-require_relative '../lib/command.rb'
-
+require_relative "../config/environment"
+require_relative "../lib/app/models/destination"
+require_relative "../lib/app/models/passenger"
+require_relative "../lib/app/models/flight"
+require_relative "../lib/command.rb"
 
 welcome
